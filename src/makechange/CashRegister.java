b347f42ba539f250cbc9ce65b97c87ee3952fef4
Desiree <desiree.vanassche@gmail.com>
2022-02-18 19:52:40 -0700

    User Story 3 Completed.

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -4,40 +4,51 @@ import java.util.Scanner;
 
 public class CashRegister {
 
+	static double tender, price, change;
+	// Create scanner to accept user input
+	static Scanner kb = new Scanner(System.in);
+
 	public static void main(String[] args) {
-			// Create scanner to accept user input
-			 Scanner kb = new Scanner(System.in);
+		// Prompt user: price of item
 
-			
-			// Prompt user: price of item
-			System.out.println("What is the price of your item? ");
-			double price = kb.nextDouble();
+		promptUser();
 
-			// Prompt user: money tendered
-			System.out.println("How much money was tendered? ");
-			double tender = kb.nextDouble();
-			
-			double change = (tender - price);
-			
-			changeLoop(); // tender - cost?? 
+		correctAmount();
+	}
 
-			// Display a message - did customer provide too little or too much $
-			// If tendered > cost, display the number of bills & coins to be returned (if else?) 
-			
+	public static void promptUser() {
+
+		System.out.println("What is the price of your item? ");
+		price = kb.nextDouble();
+
+		// Prompt user: money tendered
+		System.out.println("How much money was tendered? ");
+		tender = kb.nextDouble();
+	}
+
+	public static void correctAmount() { // check tender
+		if (tender < price) {
+			System.out.println("Not enough money tendered, please try again.");
+			promptUser();
+		} else if (tender == change) {
+			System.out.println("This is the total amount due. Have a nice day!");
 		}
 
-			//create a method for a loop - if else? 
-			public static void changeLoop() {
-				
-			}
-			
-			
-			//if (change % 4 == 0) quarter
-			// else if (change % 10 == 0) dime 
-			// else if (change % 5 == 0) nickel  
-			// else if (change % 1 == 0 penny 
+		// changeLoop(price, tender); // tender - cost??
 
+		// If tendered > cost, display the number of bills & coins to be returned (if
+		// else?)
+		// create a method for a loop - if else?
 
-
-
+		// while (change != 0) {
+		// if ()
 	}
+	// public static void changeLoop(double price, double tender) {
+	// double change = (tender - price);
+	// }
+	// if (change % 4 == 0) quarter
+	// else if (change % 10 == 0) dime
+	// else if (change % 5 == 0) nickel
+	// else if (change % 1 == 0 penny
+
+}
