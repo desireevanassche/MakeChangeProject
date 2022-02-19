74233930ed6b951c21da25e6a27ef0be5413f99f
Desiree <desiree.vanassche@gmail.com>
2022-02-18 22:16:32 -0700

    User story 4 mostly complete, comeback with fresh eyes and make corrections.

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -5,50 +5,69 @@ import java.util.Scanner;
 public class CashRegister {
 
 	static double tender, price, change;
-	// Create scanner to accept user input
-	static Scanner kb = new Scanner(System.in);
+	static int quarter, dime, nickel, penny, twenty, ten, five, one;
+	static Scanner kb = new Scanner(System.in); // create scanner
 
 	public static void main(String[] args) {
-		// Prompt user: price of item
 
-		promptUser();
+		System.out.println("What is the price of your item? "); // price prompt
+		price = (int) Math.round(kb.nextDouble() * 100); // allows to cast double to int, * 100 for more acccuracy
+															// without decimals
 
-		correctAmount();
-	}
+		System.out.println("How much money was tendered? "); // amount tendered prompt
+		tender = (int) Math.round(kb.nextDouble() * 100);
 
-	public static void promptUser() {
+		if (tender > price)
+			System.out.println("Chanage due: " + (tender - price) / 100); // must divide by 100 here since 
+																		
+		change = tender - price;
 
-		System.out.println("What is the price of your item? ");
-		price = kb.nextDouble();
+		twenty = (int) (change / 2000); // casting
 
-		// Prompt user: money tendered
-		System.out.println("How much money was tendered? ");
-		tender = kb.nextDouble();
-	}
+		change = change % 2000; // had to move decimal place over since I multiplied by 100 earlier I have to
+								// divide by 2000 now
+		
+		System.out.println(twenty + " Twenty Dollar Bills");
 
-	public static void correctAmount() { // check tender
-		if (tender < price) {
-			System.out.println("Not enough money tendered, please try again.");
-			promptUser();
-		} else if (tender == change) {
-			System.out.println("This is the total amount due. Have a nice day!");
+		ten = (int) (change / 1000);
+		if (ten > 0) {
+			change = change % 1000;
+			System.out.println(ten + " Ten Dollar Bills");
 		}
+		five = (int) (change / 500);
+		if (five > 0) {
+			change = change % 500;
+			System.out.println(five + " Five Dollar Bills");
+		}
+		one = (int) (change / 100);
+		if (one > 0) {
+			change = change % 100;
+			System.out.println(one + " One dollar Bills");
+		}
+		quarter = (int) (change / 25);
+		if (quarter > 0) {
+			change = change % 25;
+			System.out.println(quarter + " Quarters");
+		}
+		dime = (int) (change / 10);
+		if (dime > 0) {
+			change = change % 10;
+			System.out.println(dime + " Dimes");
+		}
+		nickel = (int) (change / 5);
+		if (nickel > 0) {
+			change = change % 5;
+			System.out.println(nickel + " Nickles");
+		}
+		penny = (int) (change);
+		System.out.println(penny + " Pennies");
 
-		// changeLoop(price, tender); // tender - cost??
+		if (tender < price) {
+			System.out.println("Not enough money tendered, please try again."); //not enough message 
 
-		// If tendered > cost, display the number of bills & coins to be returned (if
-		// else?)
-		// create a method for a loop - if else?
-
-		// while (change != 0) {
-		// if ()
+		}
+		else if (tender == change) {
+			System.out.println("This is the total amount due. Have a nice day!"); // correct amount message
+		}
 	}
-	// public static void changeLoop(double price, double tender) {
-	// double change = (tender - price);
-	// }
-	// if (change % 4 == 0) quarter
-	// else if (change % 10 == 0) dime
-	// else if (change % 5 == 0) nickel
-	// else if (change % 1 == 0 penny
-
 }
